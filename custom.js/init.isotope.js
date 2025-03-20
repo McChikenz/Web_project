47d8eb3eaf2a3791a3202efe29be1d8a5a017a94
McNachoz <106206847+McChikenz@users.noreply.github.com>
2025-03-20 15:13:06 -0400

    push-

--- custom.js/init.isotope.js
+++ custom.js/init.isotope.js
@@ -4,6 +4,9 @@ var $grid = $('#team-area').isotope({
   layoutMode: 'fitRows',
 });
 
+$grid.imagesLoaded().progress(function() {
+  $grid.isotope('layout');
+});
 // filter items on button click
 $('.filter-button-group').on('click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
